c7a96598480d6392cebcc5d41e76c258c332c0b1
renisalcedo <salcedoreynaldo5@gmail.com>
2019-01-29 14:56:19 -0500

    Added .gitignore Added protected isAuth function for protected routes

--- routes/form.js
+++ routes/form.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const formController = require("../controllers/form");
+const { isAuth } = require('../utils');
 
-router.get("/", formController.form);
+router.get("/", isAuth, formController.form);
 router.post("/submit", formController.createForm);
 
 module.exports = router;
