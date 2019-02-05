59e633ab5a4f35a14b8a34ca7d83a13250137a87
Erik Reyna <erik@erikreyna.com>
2019-02-05 11:48:21 -0600

    remove console.log

--- db/connection.js
+++ db/connection.js
@@ -3,7 +3,6 @@ mongoose.Promise = Promise;
 
 const { mongoConnectionString } = require("./../config/env-vars");
 
-console.log(mongoConnectionString);
 mongoose.connect(mongoConnectionString);
 
 module.exports = mongoose;
